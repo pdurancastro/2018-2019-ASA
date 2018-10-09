19fc47d7760fc60eef1f33775624a11f23f35eed
Patricia <patryharuno@gmail.com>
2018-10-09 15:43:37 +0200

    P3 palindromo

--- P3/palindromo.asm
+++ P3/palindromo.asm
@@ -3,6 +3,10 @@
 	
 	.data
 
+CadCorrecto : .asciiz "La palabra es palindromo"
+CadError: .asciiz "La palabra no es palindromo"
+
+
 cad:	.space 1024
 
 	.text
@@ -16,13 +20,13 @@ cad:	.space 1024
 	#-- Imprimir la cadena que ha introducido el usuario
 	#--(Debug)
 	
-	li $v0, 4
-	la $a0, cad
-	syscall
+	#li $v0, 4
+	#la $a0, cad
+	#syscall
 	
 	#-- S0 es el puntero. Inicialmente apunta al primer caracter de la cadena 
 	la $s0, cad
-	#-- S1 es el puntero. Apunta al 1 caracter de la cadena 
+	#-- S1 es el puntero. Apunta al 1 caracter de la cadena lo guardo porsi 
 	la $s1, cad
 	
 next:	
@@ -33,7 +37,6 @@ next:
 	li $v0, 11
 	move $a0, $t0
 	syscall
-	
 	
 	li $t1, '\n'
 	beq $t0, $t1, Puntero2
@@ -48,19 +51,54 @@ Puntero2:
 	subi $s0,$s0, 1
 	lb $t0, 0($s0)
 	
-	#-- Imprimir el caracter actual
-	li $v0, 11
-	move $a0, $t0
+	#-- Imprimir el caracter final
+	#li $v0, 11
+	#move $a0, $t0
+	#syscall
+	
+	#-- Imprimir el caracter inicial
+	#lb $t1, 0($s1)
+	#li $v0, 11
+	#move $a0, $t1
+	#syscall
+	
+	b Comparacion
+
+Comparacion:
+
+	lb $t0,0($s0)
+	lb $t1,0($s1)
+	addi $s1, $s1, 1
+	subi $s0,$s0, 1
+	
+	# Si el valor del caracter no es igual acabas
+	bne $t0,$t1, Error
+	# Si el puntero apunta al mismo lado termina el programa 
+	beq $s0,$s1, Correcto
+	
+	#s0>s1 falta la comparacion 
+	bgt $s0,$s1, Correcto
+	
+	#Comparacion de caracteres
+	#beq $t0,$t1, Comparacion, Es lo mismo que lo de abajo
+	b Comparacion
+	
+Correcto: 
+	
+	li $v0, 4
+	la $a0, CadCorrecto
 	syscall
 	
-	#-- Imprimir el caracter actual
-	lb $t1, 0($s1)
-	li $v0, 11
-	move $a0, $t1
+	b Final
+	
+Error:
+	li $v0, 4
+	la $a0, CadError
 	syscall
 	
-	
-	
+	b Final
+
+Final:		
 	li $v0, 10
 	syscall
 	
